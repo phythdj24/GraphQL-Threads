6882c873d07510f14e22bfe66fa849132b834098
Atharv Dalal <sangamdalal24@gmail.com>
2024-03-15 23:02:08 +0530

    changes

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import {ApolloServer} from '@apollo/server'
+import bodyParser from "body-parser";
+import cors from 'cors'
+import {expressMiddleware} from '@apollo/server/express4'
+
+
