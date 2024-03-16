9d4a9bb081373afc603801924ca64b840d35fb66
Atharv Dalal <sangamdalal24@gmail.com>
2024-03-16 20:49:03 +0530

    Typedefs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,43 @@
 import express from "express";
-import {ApolloServer} from '@apollo/server'
+import { ApolloServer } from "@apollo/server";
 import bodyParser from "body-parser";
-import cors from 'cors'
-import {expressMiddleware} from '@apollo/server/express4'
+import cors from "cors";
+import { expressMiddleware } from "@apollo/server/express4";
+import axios from "axios";
 
+const StartServer = async () => {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+             
+      type User {
+        id: ID!
+        name:String!
+        username:String!
+        email:String!
+        phone: String!
+      }
+           type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean
+           }
+           type Query {
+            getTodos: [Todo]
+           }
+        `,
+    
+    },
+  });
 
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(8000, () => console.log(`Server Running on PORT 8000`));
+};
+
+StartServer();
